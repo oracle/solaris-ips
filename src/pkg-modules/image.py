ad6c8421f59b7b27ffc0c2ae83de3d6fd5e8ba44
"Stephen Hahn ext:(%22) <sch@sun.com>
2007-04-09 20:36:36 -0700

    begin comments on Image object

---
+++ src/pkg-modules/image.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+
+# Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+#ident	"%Z%%M%	%I%	%E% SMI"
+
+IMG_ENTIRE = 0
+IMG_PARTIAL = 1
+IMG_USER = 2
+
+class Image(object):
+        """An Image object is a directory tree containing the laid-down contents
+        of a self-consistent graph of Packages.
+
+        An Image has a root path.
+
+        An Image of type IMG_ENTIRE does not have a parent Image.  Other Image
+        types must have a parent Image.  The external state of the parent Image
+        must be accessible from the Image's context, or duplicated within the
+        Image (IMG_PARTIAL for zones, for instance).
+
+        The parent of a user Image can be a partial Image.  The parent of a
+        partial Image must be an entire Image.
+
+        An Image of type IMG_USER stores its external state at self.root +
+        ".SUNW,pkg".
+
+        An Image of type IMG_ENTIRE or IMG_PARTIAL stores its external state at
+        self.root + "/var/pkg".
+
+        XXX Root path probably can't be absolute, so that we can combine or
+        multiply-use Image contents."""
+
+        def __init__(self, type, name, resource):
+                self.type = type
+                self.name = name
+                self.resource = version
+
+        def set_resource(self, resource):
+                return
+
