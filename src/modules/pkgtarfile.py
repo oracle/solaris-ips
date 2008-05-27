b50826aab8fbac4bff17bd7d209f16b64c7436b1
Shawn Walker <swalker@opensolaris.org>
2008-05-27 17:07:47 -0500

    2041 client performance hampered by unnecessary getpwuid/getgrgid calls

--- src/modules/pkgtarfile.py
+++ src/modules/pkgtarfile.py
@@ -21,13 +21,21 @@
 #
 
 #
-# Copyright 2007 Sun Microsystems, Inc.  All rights reserved.
+# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 #
 
 
 import os
 import tarfile
+# Without the below statements, tarfile will trigger calls to getpwuid
+# and getgrgid for every file extracted.  This in turn leads to nscd
+# usage which slows down the install phase.  Setting these attributes
+# to undefined causes tarfile to skip these calls in
+# tarfile.gettarinfo().  This information is unnecesary as it will not
+# be used by the client.
+tarfile.pwd = None
+tarfile.grp = None
 
 class PkgTarFile(tarfile.TarFile):
         """PkgTarFile is a subclass of TarFile.  It implements
