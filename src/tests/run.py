db83d04551fd5bd5467c8e5066f876d968c34f69
Brad Hall <bhall@eng.sun.com>
2008-09-03 18:16:43 -0700

    2782 Test suite should find tests to run

--- src/tests/run.py
+++ src/tests/run.py
@@ -23,27 +23,33 @@
 # Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
 # Use is subject to license terms.
 
+import os
+import sys
+# We need to execute this from the tests directory
+if os.path.basename(os.getcwd()) != "tests":
+        os.putenv('PYEXE', sys.executable)
+        os.chdir(os.path.join(os.getcwd(), "tests"))
+        import subprocess
+        cmd = [sys.executable, "run.py"]
+        cmd.extend(sys.argv[1:]) # Skip argv[0]
+        sys.exit(subprocess.call(cmd))
+
 import baseline
 import getopt
-import os
 import pkg5unittest
 import platform
 import re
 import subprocess
-import sys
 import tempfile
+import types
 import unittest
 
-# Include the current directory in the python path
-here = os.path.dirname(__file__)
-# Make sure this points to something reasonable
-if here == "":
-        here = "."
-sys.path.insert(0, here)
+# Make sure current directory is in the path
+sys.path.insert(0, ".")
 
 import cli.testutils
 if __name__ == "__main__":
-	cli.testutils.setup_environment("../../proto")
+        cli.testutils.setup_environment("../../proto")
 
 osname = platform.uname()[0].lower()
 arch = 'unknown' 
@@ -60,92 +66,45 @@ ostype = os.name
 if ostype == '':
         ostype = 'unknown'
 
-def add_tests(tests, testpat):
+def find_tests(testdir, testpat):
+        loader = unittest.TestLoader()
         suite = unittest.TestSuite()
-        pat = re.compile(".*%s.*" % testpat, re.IGNORECASE)
-        for t in tests:
-                if re.match(pat, str(t)):
-                        suite.addTest(unittest.makeSuite(t, 'test',
+        testclasses = []
+        # "testdir" will be "api", "cli", etc., so find all the files in that 
+        # directory that match the pattern "t_*.py".
+        for f in os.listdir(testdir):
+                if not (f.startswith("t_") and f.endswith(".py")):
+                        continue
+                name = os.path.join(testdir, f)
+                name = name.replace(".py", "")
+                name = '.'.join(name.split(os.path.sep))
+                try:
+                        obj = __import__(name)
+                except ImportError, e:
+                        print "Skipping %s: %s" % (name, e.__str__())
+                        continue
+                print "Loading tests from: %s" % name
+                # "api.t_filter" -> ["api", "t_filter"]
+                parts = name.split(".")
+                # subobj will be the actual file object (t_filter, etc)
+                subobj = getattr(obj, parts[1])
+                # Get all the classes from the subobj
+                for cname in dir(subobj):
+                        # Get the actual class object
+                        classobj = getattr(subobj, cname)
+                        # Make sure it's a test case
+                        if (isinstance(classobj, type) and
+                            issubclass(classobj, unittest.TestCase)):
+                                full = "%s.%s.%s" % (testdir, parts[1], cname)
+                                # Append it to our list
+                                testclasses.append((full, classobj))
+        # Only add tests if they match the test pattern
+        pat = re.compile("%s" % testpat, re.IGNORECASE)
+        for fullname, cobj in testclasses:
+                if re.search(pat, fullname):
+                        suite.addTest(unittest.makeSuite(cobj, 'test',
                             suiteClass=pkg5unittest.Pkg5TestSuite))
         return suite
-
-def make_api_tests(testpat):
-        import api.t_action
-        import api.t_catalog
-        import api.t_filter
-        import api.t_fmri
-        import api.t_imageconfig
-        import api.t_manifest
-        import api.t_misc
-        import api.t_pkgtarfile
-        import api.t_plat
-        import api.t_repositoryconfig
-        import api.t_smf
-        import api.t_version
-
-        tests = [
-            api.t_action.TestActions,
-            api.t_catalog.TestCatalog,
-            api.t_catalog.TestEmptyCatalog,
-            api.t_catalog.TestCatalogRename,
-            api.t_catalog.TestUpdateLog,
-            api.t_filter.TestFilter,
-            api.t_fmri.TestFMRI,
-            api.t_imageconfig.TestImageConfig,
-            api.t_manifest.TestManifest,
-            api.t_misc.TestMisc,
-            api.t_pkgtarfile.TestPkgTarFile,
-            api.t_plat.TestPlat,
-            api.t_repositoryconfig.TestRepositoryConfig,
-            api.t_smf.TestSMF,
-            api.t_version.TestVersion ]
-
-        return add_tests(tests, testpat)
-
-def make_cli_tests(testpat):
-        import cli.t_actions
-        import cli.t_circular_dependencies
-        import cli.t_depot
-        import cli.t_depotcontroller
-        import cli.t_image_create
-        import cli.t_info_contents
-        import cli.t_search
-        import cli.t_search_multi
-        import cli.t_pkg_install_basics
-        import cli.t_pkg_install_corrupt_image
-        import cli.t_pkgsend
-        import cli.t_pkg_list
-        import cli.t_commandline
-        import cli.t_upgrade
-        import cli.t_recv
-        import cli.t_refresh
-        import cli.t_rename
-        import cli.t_twodepot
-        import cli.t_setUp
-
-        tests = [
-            cli.t_actions.TestPkgActions,
-            cli.t_depotcontroller.TestDepotController,
-            cli.t_image_create.TestImageCreate,
-            cli.t_image_create.TestImageCreateNoDepot,
-            cli.t_info_contents.TestContentsAndInfo,
-            cli.t_depot.TestDepot,
-            cli.t_pkg_install_basics.TestPkgInstallBasics,
-            cli.t_pkg_install_corrupt_image.TestImageCreateCorruptImage,
-            cli.t_pkgsend.TestPkgSend,
-            cli.t_pkg_list.TestPkgList,
-            cli.t_commandline.TestCommandLine,
-            cli.t_upgrade.TestUpgrade,
-            cli.t_circular_dependencies.TestCircularDependencies,
-            cli.t_recv.TestPkgRecv,
-            cli.t_refresh.TestPkgRefresh,
-            cli.t_rename.TestRename,
-            cli.t_twodepot.TestTwoDepots,
-            cli.t_search.TestPkgSearch,
-            cli.t_search_multi.TestPkgSearchMulti,
-            cli.t_setUp.TestSetUp ]
-
-        return add_tests(tests, testpat)
 
 def usage():
         print >> sys.stderr, "Usage: %s [-ghpv] [-b filename] [-o regexp]" % \
@@ -168,7 +127,7 @@ if __name__ == "__main__":
                 print >> sys.stderr, "Illegal option -- %s" % e.opt
                 sys.exit(1)
 
-        bfile = os.path.join("%s" % here, "baseline.txt")
+        bfile = os.path.join(os.getcwd(), "baseline.txt")
         generate = False
         onlyval = ""
         output = pkg5unittest.OUTPUT_DOTS
@@ -192,26 +151,8 @@ if __name__ == "__main__":
                 print >> sys.stderr, "WARNING: You don't seem to be root." \
                     " Some tests may fail."
 
-        # Set-up for API tests.
-        # Include the proto directory in the python path
-        cwd = os.getcwd()
-        proto = "%s/../../proto/root_%s" % (cwd, arch)
-        pkgs = "%s/usr/lib/python2.4/vendor-packages" % proto
-        bins = "%s/usr/bin" % proto
-        sys.path.insert(1, pkgs)
-        # And make sure bins are accessible
-        os.environ["PATH"] = bins + os.pathsep + os.environ["PATH"]
-
-        api_suite = make_api_tests(onlyval)
-        cli_suite = make_cli_tests(onlyval)
-
-        try:
-                import api.t_elf
-                api_suite.addTest(unittest.makeSuite(api.t_elf.TestElf, 'test'))
-        except ImportError, e:
-                # some platforms do not have support for reading ELF files, so
-                # skip those tests if we cannot import the elf test.
-                print("NOTE: Skipping ELF tests: " + e.__str__())
+        api_suite = find_tests("api", onlyval)
+        cli_suite = find_tests("cli", onlyval)
 
         suites = []
         suites.append(api_suite)
