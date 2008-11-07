dbb5be87c7b3183936ac4f96bc04f1eb2e6fa236
John Rice <john.rice@sun.com>
2008-11-07 14:50:50 +0000

    Autocommit of backout changeset for 678

--- src/gui/data/l10n.py
+++ src/gui/data/l10n.py
@@ -1,95 +1,15 @@
 #!/usr/bin/env python
-#
-# CDDL HEADER START
-#
-# The contents of this file are subject to the terms of the
-# Common Development and Distribution License (the "License").
-# You may not use this file except in compliance with the License.
-#
-# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
-# or http://www.opensolaris.org/os/licensing.
-# See the License for the specific language governing permissions
-# and limitations under the License.
-#
-# When distributing Covered Code, include this CDDL HEADER in each
-# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
-# If applicable, add the following below this CDDL HEADER, with the
-# fields enclosed by brackets "[]" replaced with your own identifying
-# information: Portions Copyright [yyyy] [name of copyright owner]
-#
-# CDDL HEADER END
-#
-# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
-# Use is subject to license terms.
-#
-
 # The following categories are shown in GUI.
 
 def N_(message): return message
 
-l10n_categories = [
-  N_("Accessories"),
-  N_("Administration and Configuration"),
-  N_("Applets"),
-  N_("Application and Web Servers"),
-  N_("C"),
-  N_("C++"),
-  N_("Configuration and Preferences"),
-  N_("Core"),
-  N_("Databases"),
-  N_("Databases (System)"),
-  N_("Desktop"),
-  N_("Display"),
-  N_("Distribution Tools"),
-  N_("Documentation"),
-  N_("Enterprise Management"),
-  N_("File Managers"),
-  N_("File System"),
-  N_("Fonts"),
-  N_("GNOME and GTK+"),
-  N_("GNU"),
-  N_("Games"),
-  N_("Graphics and Imaging"),
-  N_("Hardware"),
-  N_("High Performance Computing"),
-  N_("Integrated Development Environments"),
-  N_("Internationalization"),
-  N_("Internet"),
-  N_("Java"),
-  N_("Libraries"),
-  N_("Libraries (System)"),
-  N_("Localizations (Desktop)"),
-  N_("Localizations (System)"),
-  N_("Media"),
-  N_("Networking"),
-  N_("Office"),
-  N_("Other Languages"),
-  N_("Other Peripherals"),
-  N_("PHP"),
-  N_("Packaging"),
-  N_("Perl"),
-  N_("Plug-ins and Run-times"),
-  N_("Ports"),
-  N_("Printing"),
-  N_("Python"),
-  N_("Ruby"),
-  N_("Scripts"),
-  N_("Security"),
-  N_("Services"),
-  N_("Sessions"),
-  N_("Shells"),
-  N_("Software Management"),
-  N_("Sound and Video"),
-  N_("Source Code Management"),
-  N_("Storage"),
-  N_("System"),
-  N_("System Utilities"),
-  N_("Text Tools"),
-  N_("Trusted"),
-  N_("Universal Access"),
-  N_("Virtualization"),
-  N_("Window Managers"),
-  N_("X11"),
-  N_("X11 (System)"),
-  None
-]
+l10n03 = N_("Communication")
+l10n04 = N_("Mail")
+l10n05 = N_("Localization")
+l10n06 = N_("Social Software")
+l10n07 = N_("Viewer")
+l10n08 = N_("RasterGraphics")
+l10n09 = N_("2DGraphics")
+l10n10 = N_("Photography")
+l10n11 = N_("CLI tools")
+l10n12 = N_("Print")
