3ba086c7794672b9c464621d97e980da844d409d
Michal Pryc <Michal.Pryc@Sun.Com>
2008-11-07 00:00:52 +0000

    4464 l10n PM and UM po files need updated (fix missing png)

---
+++ src/gui/data/l10n.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+#
+# CDDL HEADER START
+#
+# The contents of this file are subject to the terms of the
+# Common Development and Distribution License (the "License").
+# You may not use this file except in compliance with the License.
+#
+# You can obtain a copy of the license at usr/src/OPENSOLARIS.LICENSE
+# or http://www.opensolaris.org/os/licensing.
+# See the License for the specific language governing permissions
+# and limitations under the License.
+#
+# When distributing Covered Code, include this CDDL HEADER in each
+# file and include the License file at usr/src/OPENSOLARIS.LICENSE.
+# If applicable, add the following below this CDDL HEADER, with the
+# fields enclosed by brackets "[]" replaced with your own identifying
+# information: Portions Copyright [yyyy] [name of copyright owner]
+#
+# CDDL HEADER END
+#
+# Copyright 2008 Sun Microsystems, Inc.  All rights reserved.
+# Use is subject to license terms.
+#
+
+# This script generates l10n.py from opensolaris.org
+CATEGORY_FILE=opensolaris.org
+OUTPUT_FILE=l10n.py
+LANG=C
+
+export LANG
+
+printf "%s\n" "#!/usr/bin/env python" > $OUTPUT_FILE
+head -24 $0 | tail -23 >> $OUTPUT_FILE
+
+echo "
+# The following categories are shown in GUI.
+
+def N_(message): return message
+
+l10n_categories = [" \
+>> $OUTPUT_FILE
+
+cat $CATEGORY_FILE | grep "^category[ ]*=" |\
+  sed -e "s|category[ ]*=[ ]*\(.*\)|\1|" | sort | uniq |\
+  awk '{printf ("  N_(\"%s\"),\n", $0)}' \
+>> $OUTPUT_FILE
+
+printf "%s\n" "  None" >> $OUTPUT_FILE
+printf "]"             >> $OUTPUT_FILE
