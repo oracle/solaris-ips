0aa99f295e5241662d6bddfaede009723ba458c1
Brock Pytlik <bpytlik@sun.com>
2010-05-05 16:53:49 -0700

    15777 pkgdep resolve should provide an option to only resolve against the manifests provided (fix copyright)

--- src/tests/api/t_api_search.py
+++ src/tests/api/t_api_search.py
@@ -20,8 +20,7 @@
 # CDDL HEADER END
 #
 
-# Copyright 2010 Sun Microsystems, Inc.  All rights reserved.
-# Use is subject to license terms.
+# Copyright (c) 2010, Oracle and/or its affiliates. All rights reserved.
 
 import testutils
 if __name__ == "__main__":
