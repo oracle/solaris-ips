04a580f436e98829d5ac0c40b403fe2a872ada2e
Albert White <albert.white@oracle.com>
2011-05-03 12:25:48 +0100

    18190 get_release_notes_url() should reference oracle.com page not docs.sun.com

--- src/modules/misc.py
+++ src/modules/misc.py
@@ -67,7 +67,7 @@ def get_release_notes_url():
 
         # TBD: replace with a call to api.info() that can return a "release"
         # attribute of form YYYYMM against the SUNWsolnm package
-        return "http://docs.sun.com/doc/821-1479"
+        return "http://download.oracle.com/docs/cd/E19963-01/"
 
 def time_to_timestamp(t):
         """convert seconds since epoch to %Y%m%dT%H%M%SZ format"""
