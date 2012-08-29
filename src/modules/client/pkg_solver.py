55da93a178c60ffdc18a7e8618d041196fe4ce77
Shawn Walker <shawn.walker@oracle.com>
2012-08-29 16:52:33 -0700

    7194891 pkg solver operations can omit package dependencies

--- src/modules/client/pkg_solver.py
+++ src/modules/client/pkg_solver.py
@@ -1074,7 +1074,8 @@ class PkgSolver(object):
                         pass
 
                 mver = fmri.version
-                all_fmris = self.__get_catalog_fmris(fmri.pkg_name)
+                # Get a copy of the FMRI list.
+                all_fmris = self.__get_catalog_fmris(fmri.pkg_name)[:]
                 all_fmris.reverse()
 
                 # frozensets are used so callers don't inadvertently
@@ -1127,6 +1128,8 @@ class PkgSolver(object):
                                 ))
                                 remaining = frozenset(set(all_fmris) - matching)
                         else:
+                                # Get a copy of the FMRI list.
+                                all_fmris = all_fmris[:]
                                 all_fmris.reverse()
 
                                 # Iteration is performed in descending version
