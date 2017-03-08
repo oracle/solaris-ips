d40609f090f240f7e661c58ddf82c08bd6d378cf
Shawn Walker-Salas <shawn.walker@oracle.com>
2017-03-08 09:11:55 -0800

    25667890 allow downgrade of incorporation packages related to proposed pkgs (fix indent)

--- src/modules/client/pkg_solver.py
+++ src/modules/client/pkg_solver.py
@@ -3410,7 +3410,7 @@ class PkgSolver(object):
                                                 relax_all=relax_all,
                                                 depth=depth + 1)
 
-                        return candidates
+                return candidates
 
         def __allow_incorp_downgrades(self, fmri, excludes=EmptyI,
             relax_all=False):
